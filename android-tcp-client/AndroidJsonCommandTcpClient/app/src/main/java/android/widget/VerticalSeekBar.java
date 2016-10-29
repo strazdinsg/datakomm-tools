b3c6cdd48e97cd65710d6e1b24e6375541d35c3a
Girts <gist@ntnu.no>
2016-10-29 02:28:54 +0200

    Initial commit of Android TCP client (not working yet)

---
+++ android-tcp-client/AndroidJsonCommandTcpClient/app/src/main/java/android/widget/VerticalSeekBar.java
@@ -0,0 +1,8 @@
+package android.widget;
+
+/**
+ * Created by girts on 29/10/16.
+ */
+
+public class VerticalSeekBar extends SeekBar {
+}
---
+++ android-tcp-client/AndroidJsonCommandTcpClient/app/src/main/java/no/ntnu/datakomm/TcpClient.java
@@ -0,0 +1,7 @@
+package no.ntnu.datakomm;
+
+/**
+ * Created by girts on 29/10/16.
+ */
+public class TcpClient {
+}
