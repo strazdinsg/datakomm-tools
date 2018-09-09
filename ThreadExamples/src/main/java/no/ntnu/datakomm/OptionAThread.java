f873484c03da03db60b5db1c888894b30e1b95d3
Girts <gist@ntnu.no>
2018-09-09 11:54:31 +0200

    Thread examples added

---
+++ ThreadExamples/src/main/java/no/ntnu/datakomm/OptionAThread.java
@@ -0,0 +1,42 @@
+package no.ntnu.datakomm;
+
+/**
+ * This example shows one option to run code in a new thread: create a class
+ * that extends the Thread class.
+ *
+ * We simulate some long calculations to demonstrate parallel execution.
+ */
+public class OptionAThread extends Thread {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Start three calculation threads in parallel
+        Thread t1 = new OptionAThread();
+        Thread t2 = new OptionAThread();
+        Thread t3 = new OptionAThread();
+        t1.start();
+        t2.start();
+        t3.start();
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("The main thread is still running");
+            Thread.sleep(1000); // Put the current thread to sleep for 1 second.
+        }
+        System.out.println("Main thread finished work");
+    }
+
+    @Override
+    public void run() {
+        long currentThreadId = Thread.currentThread().getId();
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Thread #" + currentThreadId + " calculating something...");
+            // Sleep some random time
+            int sleepTime = 1000 + (int) (Math.random() * 2000);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException ex) {
+                System.out.println("Someone interrupted sleeping for thread"
+                        + currentThreadId);
+            }
+        }
+        System.out.println("Thread #" + currentThreadId + " finished work");
+    }
+}
---
+++ ThreadExamples/src/main/java/no/ntnu/datakomm/OptionCLambda.java
@@ -0,0 +1,69 @@
+package no.ntnu.datakomm;
+
+/**
+ * This example shows one option to run code in a new thread: create runnable
+ * methods on the fly with lambda expressions. This approach is usable if you
+ * have very custom needs to launch different operations in another thread, or
+ * you don't want your class to have yet another interface to implement.
+ */
+public class OptionCLambda {
+
+    public static void main(String[] args) throws InterruptedException {
+        // We create an object first to get access to it's methods
+        // We could use static methods as well in this example
+        OptionCLambda obj = new OptionCLambda();
+        obj.process();
+    }
+
+    public void process() throws InterruptedException {
+        // Create three threads, each of them will call a 
+        // different method of this class
+        Thread t1 = new Thread(() -> {
+            loopFive();
+        });
+        Thread t2 = new Thread(() -> {
+            loopThree();
+        });
+        Thread t3 = new Thread(() -> {
+            loop(10);
+        });
+        // Start all the threads in parallel
+        t1.start();
+        t2.start();
+        t3.start();
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Main thread is still running");
+            Thread.sleep(1000); // Put the current thread to sleep for 1 second.
+        }
+        System.out.println("Main thread finished work");
+    }
+
+    /**
+     * Simulate some long calculations
+     *
+     * @param iterationCount How many times to repeat the calculation
+     */
+    public void loop(int iterationCount) {
+        long currentThreadId = Thread.currentThread().getId();
+        for (int i = 1; i <= iterationCount; ++i) {
+            System.out.println("Thread #" + currentThreadId + " iteration " + i);
+            // Sleep some random time
+            int sleepTime = 1000 + (int) (Math.random() * 2000);
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException ex) {
+                System.out.println("Someone interrupted sleeping for thread"
+                        + currentThreadId);
+            }
+        }
+        System.out.println("Thread #" + currentThreadId + " finished work");
+    }
+
+    public void loopFive() {
+        loop(5);
+    }
+
+    public void loopThree() {
+        loop(3);
+    }
+}
