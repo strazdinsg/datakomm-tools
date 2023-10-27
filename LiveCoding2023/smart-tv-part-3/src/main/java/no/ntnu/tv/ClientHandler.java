22a29fa63f985ba5da6bb7bad86fa69a4d64bf9e
Girts Strazdins <girts.strazdins@gmail.com>
2023-10-27 09:26:19 +0200

    Refactor: introduce ClientHandler class

---
+++ LiveCoding2023/smart-tv-part-3/src/main/java/no/ntnu/tv/ClientHandler.java
@@ -0,0 +1,86 @@
+package no.ntnu.tv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import no.ntnu.message.Command;
+import no.ntnu.message.Message;
+import no.ntnu.message.MessageSerializer;
+
+/**
+ * Handler for one specific client connection (TCP).
+ */
+public class ClientHandler {
+  private final TvLogic logic;
+  private final Socket socket;
+  private final BufferedReader socketReader;
+  private final PrintWriter socketWriter;
+
+  /**
+   * Create a new client handler.
+   *
+   * @param socket Socket associated with this client
+   * @param logic  The TV logic which will process the commands
+   * @throws IOException When something goes wrong with establishing the input or output streams
+   */
+  public ClientHandler(Socket socket, TvLogic logic) throws IOException {
+    this.logic = logic;
+    this.socket = socket;
+    socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    socketWriter = new PrintWriter(socket.getOutputStream(), true);
+  }
+
+  /**
+   * Run the client handling logic.
+   */
+  public void run() {
+    Message response;
+    do {
+      Command clientCommand = readClientRequest();
+      if (clientCommand != null) {
+        System.out.println("Received a " + clientCommand.getClass().getSimpleName());
+        response = clientCommand.execute(logic);
+        if (response != null) {
+          sendResponseToClient(response);
+        }
+      } else {
+        response = null;
+      }
+    } while (response != null);
+    System.out.println("Client " + socket.getRemoteSocketAddress() + " leaving");
+  }
+
+  /**
+   * Read one message from the TCP socket - from the client.
+   *
+   * @return The received client message, or null on error
+   */
+  private Command readClientRequest() {
+    Message clientCommand = null;
+    try {
+      String rawClientRequest = socketReader.readLine();
+      clientCommand = MessageSerializer.fromString(rawClientRequest);
+      if (!(clientCommand instanceof Command)) {
+        if (clientCommand != null) {
+          System.err.println("Wrong message from the client: " + clientCommand);
+        }
+        clientCommand = null;
+      }
+    } catch (IOException e) {
+      System.err.println("Could not receive client request: " + e.getMessage());
+    }
+    return (Command) clientCommand;
+  }
+
+  /**
+   * Send a response from the server to the client, over the TCP socket.
+   *
+   * @param response The response to send to the client, NOT including the newline
+   */
+  private void sendResponseToClient(Message response) {
+    socketWriter.println(MessageSerializer.toString(response));
+  }
+
+}
--- LiveCoding2023/smart-tv-part-3/src/main/java/no/ntnu/tv/TvServer.java
+++ LiveCoding2023/smart-tv-part-3/src/main/java/no/ntnu/tv/TvServer.java
@@ -1,14 +1,8 @@
 package no.ntnu.tv;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import no.ntnu.message.Command;
-import no.ntnu.message.Message;
-import no.ntnu.message.MessageSerializer;
 
 /**
  * Handles the TCP server socket(s).
@@ -18,8 +12,6 @@ public class TvServer {
   private final TvLogic logic;
 
   boolean isTcpServerRunning;
-  private BufferedReader socketReader;
-  private PrintWriter socketWriter;
 
   public TvServer(TvLogic logic) {
     this.logic = logic;
@@ -34,10 +26,9 @@ public class TvServer {
     if (listeningSocket != null) {
       isTcpServerRunning = true;
       while (isTcpServerRunning) {
-        Socket clientSocket = acceptNextClientConnection(listeningSocket);
-        if (clientSocket != null) {
-          System.out.println("New client connected from " + clientSocket.getRemoteSocketAddress());
-          handleClient(clientSocket);
+        ClientHandler clientHandler = acceptNextClientConnection(listeningSocket);
+        if (clientHandler != null) {
+          clientHandler.run();
         }
       }
     }
@@ -53,65 +44,15 @@ public class TvServer {
     return listeningSocket;
   }
 
-  private Socket acceptNextClientConnection(ServerSocket listeningSocket) {
-    Socket clientSocket = null;
+  private ClientHandler acceptNextClientConnection(ServerSocket listeningSocket) {
+    ClientHandler clientHandler = null;
     try {
-      clientSocket = listeningSocket.accept();
-      socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      socketWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-
+      Socket clientSocket = listeningSocket.accept();
+      System.out.println("New client connected from " + clientSocket.getRemoteSocketAddress());
+      clientHandler = new ClientHandler(clientSocket, logic);
     } catch (IOException e) {
       System.err.println("Could not accept client connection: " + e.getMessage());
     }
-    return clientSocket;
+    return clientHandler;
   }
-
-  private void handleClient(Socket clientSocket) {
-    Message response;
-    do {
-      Command clientCommand = readClientRequest();
-      if (clientCommand != null) {
-        System.out.println("Received a " + clientCommand.getClass().getSimpleName());
-        response = clientCommand.execute(logic);
-        if (response != null) {
-          sendResponseToClient(response);
-        }
-      } else {
-        response = null;
-      }
-    } while (response != null);
-    System.out.println("Client " + clientSocket.getRemoteSocketAddress() + " leaving");
-  }
-
-  /**
-   * Read one message from the TCP socket - from the client.
-   *
-   * @return The received client message, or null on error
-   */
-  private Command readClientRequest() {
-    Message clientCommand = null;
-    try {
-      String rawClientRequest = socketReader.readLine();
-      clientCommand = MessageSerializer.fromString(rawClientRequest);
-      if (!(clientCommand instanceof Command)) {
-        if (clientCommand != null) {
-          System.err.println("Wrong message from the client: " + clientCommand);
-        }
-        clientCommand = null;
-      }
-    } catch (IOException e) {
-      System.err.println("Could not receive client request: " + e.getMessage());
-    }
-    return (Command) clientCommand;
-  }
-
-  /**
-   * Send a response from the server to the client, over the TCP socket.
-   *
-   * @param response The response to send to the client, NOT including the newline
-   */
-  private void sendResponseToClient(Message response) {
-    socketWriter.println(MessageSerializer.toString(response));
-  }
-
 }
